f726452a1f3a89079062727b6f2b8cc070e04f00
abbasiiqra <abbasiiqra44@gmail.com>
2025-02-15 00:27:26 +0530

    new

--- src/api/users/models/users.js
+++ src/api/users/models/users.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      console.log("Creating a new entry:", data);
+    },
+  },
+};
