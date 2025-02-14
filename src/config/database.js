a9beb3a4dcc42f87ad1a306550dfc3471b96028f
abbasiiqra <abbasiiqra44@gmail.com>
2025-02-15 01:11:31 +0530

    sf

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: "mongo",
-    connection: {
-      uri: env("DATABASE_URI"),
-      ssl: env.bool("DATABASE_SSL", true),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        client: "mongo",
+        uri: env("DATABASE_URI", "mongodb://localhost:27017/strapi"),
+      },
+      options: {
+        ssl: env.bool("DATABASE_SSL", false),
+      },
     },
-    debug: false,
   },
 });
